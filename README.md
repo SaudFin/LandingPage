656a3d2b80500549e0fab1fbe9b78f37c4616295
SaudFin <developer.saud@gmail.com>
2020-10-07 11:58:20 +0300

    ready to be submitted

--- README.md
+++ README.md
@@ -4,16 +4,13 @@ This project is the second of FEND-Udacity projects, and it includes Javascript.
 
 ## My approach to the project
 
-_1-Built the HTML & CSS first_
-
-_2-used queryselector to select all `<section>` elements_
-
-_3-used for..of loop to iterate through array-like object (nodelist)_
-
-_4-added event listeners to each element_
+- build two functions to active section and inactive section
+- build function to create navigation links
+- combined functions into for..of loop that would the first two functions to every section and call linkCreator function and pass it a section
 
 ## Resources
 
 I had a bug and I used stackoverflow to get some help _find the link below_
 
 [StackOverFlow](https://stackoverflow.com/questions/64221169/functions-fires-regardless-of-the-eventlisteners/64221353?noredirect=1#comment113564632_64221353)
+[StackOverFlow](https://stackoverflow.com/questions/64238843/eventlisteners-dont-function-proparly-because-javascript-returns-object-htmlel/64239601#64239601)
--- js/app.js
+++ js/app.js
@@ -1,71 +1,38 @@
-// const sections = document.querySelectorAll("section");
+let sections = document.querySelectorAll("section");
+let list = document.getElementById("navbar__list");
 
-// // this function changes the background of the section to show it is the active one
-// const activeSection = section => {
-//   section.style.backgroundColor = "#6BCAE2";
-// };
+// this function changes the background of the section to show it is the active one
+const activeSection = section => {
+  section.className = "your-active-class";
+};
 
-// // this function return the section to its normal status when the user is not viewing it anymore
-// const inactiveSection = section => {
-//   section.style.backgroundColor = "";
-// };
+// this function return the section to its normal status when the user is not viewing it anymore
+const inactiveSection = section => {
+  section.className = "";
+};
 
-// // to loop throught the nodelist and add eventlistneres for the functions above
-// for (let section of sections) {
-//   section.addEventListener("mouseover", () => {
-//     activeSection(section);
-//   });
-//   section.addEventListener("mouseout", () => {
-//     inactiveSection(section);
-//   });
-// }
+// this function to be called when navigation links are needed to be created
+const linkCreator = section => {
+  let point = document.createElement("li");
+  let link = document.createElement("a");
+  link.className = "menu__link";
+  link.textContent = section.id;
+  // to make it look clickable
+  link.href = `#${section.id}`;
+  link.addEventListener("click", () => {
+    section.scrollIntoView(true);
+  });
+  point.appendChild(link);
+  return point;
+};
 
-/**
- *
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- *
- * Dependencies: None
- *
- * JS Version: ES2015/ES6
- *
- * JS Standard: ESlint
- *
- */
+for (let section of sections) {
+  section.addEventListener("mouseover", () => {
+    activeSection(section);
+  });
+  section.addEventListener("mouseout", () => {
+    inactiveSection(section);
+  });
 
-/**
- * Define Global Variables
- *
- */
-
-/**
- * End Global Variables
- * Start Helper Functions
- *
- */
-
-/**
- * End Helper Functions
- * Begin Main Functions
- *
- */
-
-// build the nav
-
-// Add class 'active' to section when near top of viewport
-
-// Scroll to anchor ID using scrollTO event
-
-/**
- * End Main Functions
- * Begin Events
- *
- */
-
-// Build menu
-
-// Scroll to section on link click
-
-// Set sections as active
+  list.appendChild(linkCreator(section));
+}
